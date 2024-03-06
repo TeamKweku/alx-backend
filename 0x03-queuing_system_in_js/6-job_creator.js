9dede0a6d08b15fe23ae328a6fbf0fdaaf04da0c
teamkweku <teamkweku@outlook.com>
2024-03-06 10:36:36 +0000

    creeating jobs with kue in nodejs

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,20 @@
+import { createQueue } from 'kue';
+
+const queue = createQueue({ name: 'push_notification_code' });
+
+const job = queue.create('push_notification_code', {
+  phoneNumber: '05476565454',
+  message: 'Account registered successfully '
+});
+
+job
+  .on('enqueue', () => {
+    console.log('Notification job created:', job.id);
+  })
+  .on('complete', () => {
+    console.log('Notification job completed');
+  })
+  .on('failed attempt', () => {
+    console.log('Notification job failed');
+  });
+job.save();
